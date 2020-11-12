d57f304468db677c24b185ed3a420015b8dd09f3
zhangai <zhangai@xiaomi.com>
2020-11-12 16:55:28 +0800

    管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    // entry: './src/index.js',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
@@ -15,7 +14,6 @@ module.exports = {
         })
     ],
     output: {
-        // filename:'bundle.js',
         filename:'[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
