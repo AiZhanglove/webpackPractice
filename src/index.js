e809973639acba463f210a1794b86a0af2c12f39
zhangai <zhangai@xiaomi.com>
2020-11-12 16:39:50 +0800

    设定 HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import printMe from './print.js';
+ 
 
 
 function component(){
     var element = document.createElement('div');
-    // Lodash, now import by this script;
     element.innerHTML = _.join(['Hello', 'webpack',]);
+
+
+    var btn = document.createElement('button');
+    // Lodash, now import by this script;
+
+    btn.innerHTML = "点击并测试print console!"
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+
 
     return element
 }
