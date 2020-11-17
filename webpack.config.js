743d362db09cb7d6ec606fd8bfecd36c68d3011e
zhangai <zhangai@xiaomi.com>
2020-11-17 10:44:09 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { webpack } = require('webpack');
+    
+console.log( webpack )
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        // print: './src/print.js'
     },
     devtool: 'inline-source-map', // 映射错误发生的源文件位置
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true,
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: '输出管理'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename:'[name].bundle.js',
