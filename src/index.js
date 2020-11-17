a490a2f01b0846cc9a328f8dd612d2619246e438
zhangai <zhangai@xiaomi.com>
2020-11-17 19:48:35 +0800

    热更新和treeShaking

--- src/index.js
+++ src/index.js
@@ -1,28 +1,44 @@
-import _ from 'lodash';
-import printMe from './print.js';
+// import _ from 'lodash';
+// import printMe from './print.js';
+// import './styles.css';
+
+// function component(){
+//     var element = document.createElement('div');
+//     element.innerHTML = _.join(['Hello', 'webpack',]);
 
 
+//     var btn = document.createElement('button');
+//     // Lodash, now import by this script;
+
+//     btn.innerHTML = "点击并测试print console!"
+//     btn.onclick = printMe;
+//     element.appendChild(btn);
+
+
+
+//     return element
+// }
+// // document.body.appendChild(component());
+// let element  = component();
+// document.body.appendChild(element);
+
+// if (module.hot){
+//     module.hot.accept('./print.js',function(){
+//         console.log('Accepting the updated printMe module!');
+//         // printMe();
+//         document.body.removeChild(element);
+//         element = component();
+//         document.body.appendChild(element);
+//     })
+// }
+
+import {cube} from './math.js';
 function component(){
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack',]);
-
-
-    var btn = document.createElement('button');
-    // Lodash, now import by this script;
-
-    btn.innerHTML = "点击并测试print console!"
-    btn.onclick = printMe;
-    element.appendChild(btn);
-
-
-
-    return element
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'hello webpack',
+        '6 cubed is equal to' + cube(6)
+    ].join('\n\n');
+    return element;
 }
 document.body.appendChild(component());
-
-if (module.hot){
-    module.hot.accept('./print.js',function(){
-        console.log('Accepting the updated printMe module!');
-        printMe();
-    })
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { webpack } = require('webpack');
+const webpack = require('webpack');
     
-console.log( webpack )
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-        // print: './src/print.js'
-    },
-    devtool: 'inline-source-map', // 映射错误发生的源文件位置
+    // entry: {
+    //     app: './src/index.js',
+    //     print: './src/print.js'
+    // },
+    entry: './src/index.js',
+    // devtool: 'inline-source-map', // 映射错误发生的源文件位置
     devServer: {
         contentBase: './dist',
         hot: true,
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -28,5 +36,5 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     },
-    
+    mode: "production", // 压缩文件 --optimize-minimize 标记也会在 webpack 内部调用 UglifyJsPlugin
 };
