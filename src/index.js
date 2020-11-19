92eeb3b3a6d2e3264209c899b170092978a58986
zhangai <zhangai@xiaomi.com>
2020-11-19 14:24:19 +0800

    add

--- src/index.js
+++ src/index.js
@@ -32,13 +32,28 @@
 //     })
 // }
 
-import {cube} from './math.js';
-function component(){
-    var element = document.createElement('pre');
-    element.innerHTML = [
-        'hello webpack',
-        '6 cubed is equal to' + cube(6)
-    ].join('\n\n');
-    return element;
-}
-document.body.appendChild(component());
+// import {cube} from './math.js';
+// if(process.env.NODE_ENV !== 'production') {
+//     console.log('Looks like we are in development mode!')
+// }
+// function component(){
+//     var element = document.createElement('pre');
+//     element.innerHTML = [
+//         'hello webpack',
+//         '6 cubed is equal to' + cube(6)
+//     ].join('\n\n');
+//     return element;
+// }
+// document.body.appendChild(component());
+
+function getComponent() {
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
+    }
+    
+     getComponent().then(component => {
+       document.body.appendChild(component);
+     })
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
+// const webpack = require('webpack');
     
 
 module.exports = {
-    // entry: {
-    //     app: './src/index.js',
-    //     print: './src/print.js'
-    // },
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        // another: './src/another-module.js'
+      },
+    // entry: './src/index.js',
     // devtool: 'inline-source-map', // 映射错误发生的源文件位置
     devServer: {
         contentBase: './dist',
@@ -26,15 +26,23 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: '输出管理'
-        }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+            title: 'Code Splitting'
+          }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common' // 指定公共 bundle 的名称。
+        // })
+        
+        // new HtmlWebpackPlugin({
+        //     title: '输出管理'
+        // }),
+        // new webpack.NamedModulesPlugin(),
+        // new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename:'[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        // publicPath: '/'
     },
-    mode: "production", // 压缩文件 --optimize-minimize 标记也会在 webpack 内部调用 UglifyJsPlugin
+    // mode: "production", // 压缩文件 --optimize-minimize 标记也会在 webpack 内部调用 UglifyJsPlugin
 };
